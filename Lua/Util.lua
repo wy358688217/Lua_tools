eb0eea26b6f968ed8312d33b5e67a926e0c1b54f
Mr.Wang <wang@WangdeMacBook-Pro.local>
2016-08-22 17:42:28 +0800

    Url encode
    %22XX%22

--- Lua/Util.lua
+++ Lua/Util.lua
@@ -26,34 +26,11 @@ function Util:print_table( _table , _name )
 	print("]")
 end
 
-function Util:sz_T2S(_t)
-    local szRet = "{"
-    function doT2S(_i, _v)
-        if "number" == type(_i) then
-            szRet = szRet .. "[" .. _i .. "] = "
-            if "number" == type(_v) then
-                szRet = szRet .. _v .. ","
-            elseif "string" == type(_v) then
-                szRet = szRet .. '"' .. _v .. '"' .. ","
-            elseif "table" == type(_v) then
-                szRet = szRet .. sz_T2S(_v) .. ","
-            else
-                szRet = szRet .. "nil,"
-            end
-        elseif "string" == type(_i) then
-            szRet = szRet .. '["' .. _i .. '"] = '
-            if "number" == type(_v) then
-                szRet = szRet .. _v .. ","
-            elseif "string" == type(_v) then
-                szRet = szRet .. '"' .. _v .. '"' .. ","
-            elseif "table" == type(_v) then
-                szRet = szRet .. sz_T2S(_v) .. ","
-            else
-                szRet = szRet .. "nil,"
-            end
-        end
-    end
-    table.foreach(_t, doT2S)
-    szRet = szRet .. "}"
-    return szRet
+function Util:encode( _table )
+	if type(_table) == "table" then
+	  for k, v in pairs(_table) do
+	  	json = k.."="..v..","
+		  return k.."="..v
+	  end
+	end
 end
--- Lua/http_test.lua
+++ Lua/http_test.lua
@@ -1,6 +1,6 @@
 local http = require("socket.http")
 local ltn12 = require("ltn12")
-
+ 
 function http.get(u)
    local t = {}
    local r, c, h = http.request{
@@ -8,10 +8,10 @@ function http.get(u)
       sink = ltn12.sink.table(t)}
    return r, c, h, table.concat(t)
 end
-
- url = "http://www.baidu.com"
- r,c,h,body=http.get(url)
-if c~= 200 then
-     return
-end
-print(body)
+ 
+-- url = "http://www.baidu.com"
+-- r,c,h,body=http.get(url)
+-- if c~= 200 then
+--     return
+-- end
+-- print(body)
